fb88465b9a3ffe2e5f5efa4c63b4ec89342e6377
Theethawat Savastham <5910110150@psu.ac.th>
2020-06-02 16:48:34 +0700

    debug code

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let url =
   clusterURL +
   "/" +
   "?retryWrites=true"
+console.log("Username " + username + "  Cluster URL " + clusterURL)
 // Access Control Origin
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
