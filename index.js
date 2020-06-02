5da1979a1e080972b6a8707b5c5d4b404a2bc65c
Theethawat Savastham <5910110150@psu.ac.th>
2020-06-02 14:07:47 +0700

    Using ENV and config deploy script

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express")
 const MongoClient = require("mongodb").MongoClient
+const dotenv = require("dotenv")
 const assert = require("assert")
-const connectionItem = require("./env.json")
-const port = 8030
+const port = 80
+dotenv.config()
 let app = express()
+let username = process.env.ACCESS_USER_NAME
+let password = process.env.ACCESS_PASSWORD
+let clusterURL = process.env.MONGO_URL
 
 let url =
   "mongodb://" +
-  connectionItem.username +
+  username +
   ":" +
-  connectionItem.password +
+  password +
   "@" +
-  connectionItem["cluster-url"] +
+  clusterURL +
   "/" +
   "?retryWrites=true"
-
 // Access Control Origin
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
